86016fe9bccf634bbd5f1e58815de4330d5dd90c
menm91 <menm91@qq.com>
2017-10-10 10:01:03 +0800

    添加线程的实例

---
+++ JavaThinking/src/me/qianlv/thread/Interrupting2.java
@@ -0,0 +1,42 @@
+package me.qianlv.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+class BlockedMutex {
+    private ReentrantLock lock = new ReentrantLock();
+
+    public BlockedMutex() {
+        lock.lock();
+    }
+
+    public void f() {
+        try {
+            lock.lockInterruptibly();
+            System.out.println("lock acquired in f()");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted from lock acquisition in f()");
+        }
+    }
+}
+
+class Blocked2 implements Runnable {
+    BlockedMutex blockedMutex = new BlockedMutex();
+
+    @Override
+    public void run() {
+        System.out.println("Waiting fro f() in BlockedMutex");
+        blockedMutex.f();
+        System.out.println("Broken of blocked call");
+    }
+}
+
+public class Interrupting2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new Blocked2());
+        t.start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Issuing t.interrupt()");
+        t.interrupt();
+    }
+}
